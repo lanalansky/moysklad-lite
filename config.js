a1a64de20909fc12a2fdb4191664ecd6861a08c6
lana <treason23@gmail.com>
2026-09-09 20:18:29 +0400

    Add Apps Script Web App URL to config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Вставьте сюда URL вашего Google Apps Script Web App (см. README.md, шаг 3).
 const CONFIG = {
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE"
+  API_URL: "https://script.google.com/macros/s/AKfycbxLE9D7jXNEHyDILcPn9uUMQQAKmxNz-QCzj2u6-J72PbqvelgEuWWCJ-xgMMmJB41hFg/exec"
 };
